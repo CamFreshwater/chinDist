dbba11d4907937b8f2c3bb64e1faee82665f5252
Cameron Freshwater <ccfreshwater@gmail.com>
2019-07-10 11:42:28 -0700

    plot high seas catch locations

--- R/highSeasCatch.R
+++ R/highSeasCatch.R
@@ -18,6 +18,7 @@ adDat <- adDatFull %>%
   filter(PROB_1 > 0.5, 
          # remove non-WCVI capture locations
          START_LAT < 51,
+         Month %in% c("MAY", "JUN", "JUL", "AUG", "SEP"),
          !str_detect(STATION, "BR"),
          !str_detect(STATION, "JS"),
          !str_detect(STATION, "QCS"),
@@ -50,8 +51,44 @@ ggplot(data = nAm, mapping = aes(x = long, y = lat, group = group)) +
 adDatTrim <- adDat %>% 
   group_by(REGION_1) %>%
   filter(n() > 4) %>% 
-  group_by(REGION_1, Year) %>% 
+  group_by(REGION_1, HSS_REGION_1, Year) %>% 
   summarize(catch = length(REGION_1))
 
-ggplot(adDatTrim, aes(x = Year, y = catch, fill = REGION_1)) +
-  geom_bar(stat = "identity")
+
+plotTheme <- theme(legend.title = element_text(colour = "grey30", 
+                                               size = rel(0.8)),
+                   panel.border = element_rect(fill = NA, colour = "grey70", 
+                                               size = 1),
+                   legend.key.size = unit(1, "lines"),
+                   legend.text = element_text(size = rel(0.6), colour = "grey30"))
+
+png(here::here("figs", "highSeasCatch", "catchByStock.png"), height = 2.5,
+    width = 6, units = "in", res = 300)
+ggplot(adDatTrim, aes(x = Year, y = catch, fill = HSS_REGION_1)) +
+  geom_bar(stat = "identity") +
+  plotTheme
+dev.off()
+
+
+# Maps by stock
+adDatTrim2 <- adDat %>% 
+  group_by(REGION_1) %>%
+  filter(n() > 4) %>% 
+         # ,
+         # !REGION_1 == "PUGET SOUND") %>% 
+  mutate(HSS_REGION_1 = fct_recode(HSS_REGION_1, 
+                               OREGON_NC = "OREGON NORTH & CENTRAL")) %>% 
+  ungroup()
+
+png(here::here("figs", "highSeasCatch", "catchByStockMap.png"), height = 4,
+    width = 6, units = "in", res = 300)
+ggplot(data = nAm, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(xlim = c(-129, -123), ylim = c(48, 51), ratio = 1.3) + 
+  geom_polygon(color = "black", fill = "gray80") +
+  geom_jitter(data = adDatTrim2, 
+              aes(x = START_LONG, y = START_LAT, colour = as.factor(Year)), 
+              inherit.aes = FALSE, width = 0.05, height = 0.05) + 
+  labs(x = "Longitude", y = "Latitude") +
+  samSim::theme_sleekX(legendSize = 0.7) +
+  facet_wrap(~HSS_REGION_1)
+dev.off()
