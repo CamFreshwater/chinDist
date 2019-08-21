d02e132efbc3ddbdea69f29ad47e32bb56641ae0
Cameron Freshwater <ccfreshwater@gmail.com>
2019-08-21 14:16:38 -0700

    summarize SC catch data

--- R/southCoast/gsiExplore.R
+++ R/southCoast/gsiExplore.R
@@ -12,6 +12,46 @@ sqlTables(con, tableType = "TABLE")$TABLE_NAME
 
 # Import majority of data directly from tables because they aren't linked with
 # one another, limiting the utility of SQL queries that include joins
+
+# all gsi samples
+sampInvQry <- "SELECT *
+               FROM [Sample Inventory]"
+sampInv <- sqlQuery(con, sampInvQry) %>% 
+  select(-F14, -F15, -F16) %>% 
+  rename_all(list(~make.names(.))) #get rid of spaces in col names
+trimSampInv <- sampInv %>% 
+  select(Sample.ID, Vial.ID, Year, Fishery, Catch.Region, Catch.Area, 
+         Month.Name, Lab.Reported.N)
+
+# stock composition data
+stockCompQry <- "SELECT *
+                 FROM [Stock Level Results] 
+                 INNER JOIN [Stocks with Region Codes]
+                 ON [Stock Level Results].[Stock Code] = 
+                  [Stocks with Region Codes].[Stock Code];"
+stockComp <- sqlQuery(con, stockCompQry) %>% 
+  rename_all(list(~make.names(.))) %>% 
+  left_join(., trimSampInv, by = "Sample.ID") %>% 
+  select(Sample.ID:SD, Region1Code:Lab.Reported.N) %>% 
+  filter(Fishery == "Area G Com")
+yrs <- unique(stockComp$Year)
+
+# confirm one sample collected per catch region, year, and month
+# dum <- stockComp %>% 
+#   select(-c(Stock.Code:Vial.ID)) %>% 
+#   distinct() %>% 
+#   group_by(Catch.Region, Year, Month.Name) %>% 
+#   tally()
+
+regQry <- "SELECT *
+           FROM [Stocks with Region Codes]
+           LEFT JOIN [Region 2 Stock Names]
+           ON [Stocks with Region Codes].Region2Code = 
+              [Region 2 Stock Names].Region2Code;"
+regKey <- sqlQuery(con, regQry) %>% 
+  select(ID:FraserGroupCode, Region2Name)
+head(regKey)
+
 # all catch/effort data
 catchQry <- "SELECT [Area G FOS Catch Estimates].ESTIMATE_TYPE, 
                     [Area G FOS Catch Estimates].LICENCE_AREA, 
@@ -30,50 +70,21 @@ catchQry <- "SELECT [Area G FOS Catch Estimates].ESTIMATE_TYPE,
                     [Area G FOS Catch Estimates].CHINOOK_KEPT, 
                     [Area G FOS Catch Estimates].CHINOOK_RELD, 
                     [Area G FOS Catch Estimates].COMMENTS
-               FROM [Area G FOS Catch Estimates] INNER JOIN [Management Regions] 
-                ON [Area G FOS Catch Estimates].MGMT_AREA = 
+               FROM [Area G FOS Catch Estimates] 
+               INNER JOIN [Management Regions] 
+               ON [Area G FOS Catch Estimates].MGMT_AREA = 
                   [Management Regions].[Managment Area]
                WHERE ((([Area G FOS Catch Estimates].TARGETS_CHINOOK)='Yes'));
 "
-areaCatch <- sqlQuery(con, catchQry)
-head(areaCatch)
+areaCatch <- sqlQuery(con, catchQry) %>% 
+  rename_all(list(~make.names(.))) %>%
+  filter(FISHING.YEAR %in% yrs)
 
-# all gsi samples
-sampInvQry <- "SELECT *
-               FROM [Sample Inventory]"
-sampInv <- sqlQuery(con, sampInvQry) %>% 
-  select(-F14, -F15, -F16) %>% 
-  rename_all(list(~make.names(.))) %>% #get rid of spaces in col names
-  filter(!Fishery == "Area G Subleg")
-trimSampInv <- sampInv %>% 
-  select(Sample.ID, Vial.ID, Year, Fishery, Catch.Region, Catch.Area, 
-         Month.Name, Lab.Reported.N)
+#summarize catches by area, month and year to match GSI data
+sumCatch <- areaCatch %>% 
+  group_by(MGMT_AREA, FISHING.MONTH, FISHING.YEAR) %>% 
+  summarize(sumCatch = sum(CHINOOK_KEPT),
+            sumEffort = sum(VESSELS_OP)) %>% 
+  select(MGMT_AREA, FISHING.MONTH, FISHING.YEAR, sumCatch, sumEffort)
+head(sumCatch)
 
-# stock composition data
-stockCompQry <- "SELECT *
-                 FROM [Stock Level Results] 
-                 INNER JOIN [Stocks with Region Codes]
-                 ON [Stock Level Results].[Stock Code] = 
-                  [Stocks with Region Codes].[Stock Code];"
-stockComp <- sqlQuery(con, stockCompQry) %>% 
-  rename_all(list(~make.names(.))) %>% 
-  left_join(., trimSampInv, by = "Sample.ID") 
-# %>% 
-  # select(Sample.ID:SD, Stock.Name:Lab.Reported.N)
-
-dum <- stockComp %>% filter(is.na(Year))
-dum71 <- stockComp %>% filter(Sample.ID == "71")
-
-dum %>% 
-  filter(is.na(Vial.ID)) %>% 
-  select(Sample.ID) %>% 
-  distinct()
-
-regQry <- "SELECT *
-           FROM [Region 2 Sample Results] 
-           INNER JOIN [Region 2 Stock Names] 
-           ON [Region 2 Sample Results].[Region 2 Code] = 
-            [Region 2 Stock Names].Region2Code;"
-regComp <- sqlQuery(con, regQry) %>% 
-  rename_all(list(~make.names(.))) %>% 
-  select(Sample.ID, Region2Code, Region2Name, Estimate, SD)
