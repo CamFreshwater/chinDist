54a817324d9b2289c88ad3dd23d4d6b1e8e9e736
Cameron Freshwater <ccfreshwater@gmail.com>
2019-09-17 12:20:40 -0400

    clean ind. prob data

--- R/southCoast/gsiIndProbsClean.R
+++ R/southCoast/gsiIndProbsClean.R
@@ -5,16 +5,46 @@
 library(tidyverse)
 library(ggplot2)
 
-dat <- read.csv(here::here("data", "gsiCatchData", "commTroll", 
+datRaw <- read.csv(here::here("data", "gsiCatchData", "commTroll", 
                            "wcviIndProbs_trim.csv"), 
                 stringsAsFactors = FALSE)
 
 # pull ID column, split and rename
-xy <- data.frame(t(matrix(unlist(strsplit(as.vector(dat$sampleID), 
+id <- data.frame(t(matrix(unlist(strsplit(as.vector(datRaw$sampleID), 
                                           split = " ")),
-                        nrow = 5, ncol = length(dat$sampleID)))) %>% 
+                        nrow = 5, ncol = length(datRaw$sampleID)))) %>% 
   rename("statArea" = X1, "year" = X2, "gear" = X3, "jDay" = X4, 
-         "fishNum" = X5) 
+         "fishNum" = X5) %>% 
+  mutate(statArea = case_when(
+            statArea %in% c("Area023", "Area23", "Area_23") ~ "23",
+            statArea %in% c("Area123", "Area123SWVI", "Area123Comm") ~ "123",
+            statArea %in% c("Area124", "Area124SWVI", "Area124Comm") ~ "124",
+            statArea %in% c("Area125", "Area125NWVI") ~ "125",
+            statArea %in% c("Area126", "Area126NWVI") ~ "126",
+            statArea %in% c("Area127", "Area127NWVI") ~ "127",
+            statArea %in% c("Area026") ~ "26",
+            statArea %in% c("Area24", "Area_24", "Area124_24", 
+                            "Area_24xgill") ~ "24",
+            TRUE ~ as.character(statArea)),
+         abbYear = sapply(strsplit(as.character(year), '[()]'), 
+                       function(x) (x)[2]),
+         year = paste("20", abbYear, sep = ""),
+         date = as.Date(as.numeric(as.character(jDay)), 
+                        origin = as.Date(paste(year, "01", "01", sep = "-"))),
+         month = lubridate::month(as.POSIXlt(date, format="%Y-%m-%d")))
 
+dat <- cbind(id, datRaw) %>% 
+  select(-sampleID, -abbYear) %>% 
+  # for now remove ~150 fish that can't be assigned to an individual stat area
+  filter(!statArea %in% c("Area123-124", "Area125-126", "Area126-127", 
+                          "Area124_24"))
 
-dum <- sapply(strsplit(as.character(xy$year), "()"), function(x) print(x)[2:3])
+temp <- dat %>% 
+  group_by(statArea, year, month) %>% 
+  tally()
+
+summDat <- expand.grid(unique(dat$statArea), unique(dat$year), unique(dat$month)) %>%
+  rename("statArea" = Var1, "year" = Var2, "month" = Var3) %>% 
+  left_join(., temp, by = c("statArea", "year", "month")) %>% 
+  replace_na(list(n = 0))
+
