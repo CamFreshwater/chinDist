614317dec323be903c3ebe913c224ce730314c40
CamFreshwater <ccfreshwater@gmail.com>
2020-03-20 09:21:33 -0700

    try combining GAMMs and multinomial; unlikely to work...

--- R/southCoast/combine_models.R
+++ R/southCoast/combine_models.R
@@ -9,38 +9,35 @@ catch <- readRDS(here::here("data", "gsiCatchData", "commTroll",
                             "nbgam_preds.RDS"))
 comp <- readRDS(here::here("data", "gsiCatchData", "commTroll",
                            "multinomial_preds.RDS")) %>% 
-  mutate(month_n = as.numeric(as.character(month))) %>%
   rename(area = statArea) %>% 
+  mutate(month_n = as.numeric(as.character(month)),
+         id = paste(stock, month, area, sep = "_")) %>%
   left_join(., catch, by = c("month_n", "area")) %>% 
   select(-c(stock_n, ests, pred_prob_low, pred_prob_up, month_n, low, high))
 
-comp %>% 
-  select(pred_prob, mu.pred) %>% 
-  cov()
-
-calcSEs <- function(x, y) {
-  rs_riv <- x[x$stage == "river", ]$mean
-  
-  x$final_mu <- NA
-  x$final_se <- NA
-  for (i in 1:nrow(x)) {
-    if (x$stage[i] == "river") {
-      x$final_mu[i] <- rs_riv
-      x$final_se[i] <- x$se[i] 
-    }
-    if (x$stage[i] == "est" | x$stage[i] == "microtroll") {
-      stg <- as.character(x$stage[i])
-      cov_m <- y[c(stg, "river"), c(stg, "river")]
-      
-      x$final_mu[i] <- rs_riv / x[[i, "mean"]]
-      x$final_se[i] <- RMark::deltamethod.special("prod",
-                                                  #for vector of means pass
-                                                  #untransformed value 
-                                                  mean = c(1 / x[[i, "mean"]],
-                                                           rs_riv),
-                                                  cov = cov_m, 
-                                                  ses = TRUE) 
-    }
-  }
-  return(x)
+# for now assume 0 covariance among catch and stock composition parameters
+calc_se <- function(x_mu, x_se, y_mu, y_se, covar = 0) {
+  cov_mat <- matrix(c(x_se, covar, covar, y_se), nrow = 2, byrow = TRUE)
+  RMark::deltamethod.special("prod", mean = c(x_mu, y_mu), cov = cov_mat,
+                             ses = TRUE)
 }
+
+out <- comp %>% 
+  group_by(id) %>% 
+  mutate(mu_k = pred_prob * mu.pred,
+         se_k = calc_se(pred_prob, pred_prob_se, mu.pred, se.pred),
+         mu_k_low = mu_k + (qnorm(0.025) * se_k),
+         mu_k_up = mu_k + (qnorm(0.975) * se_k)) 
+
+out %>% 
+  filter(area == "123",
+         month == "1")
+
+# plot
+ggplot(out) +
+  geom_point(aes(x = as.factor(month), y = mu.pred, fill = area), 
+             shape = 21) +
+  # geom_pointrange(aes(x = as.factor(month), y = pred_prob, ymin = pred_prob_low, 
+  #                     ymax = pred_prob_up, fill = statArea), shape = 21) +
+  labs(y = "Probability", x = "Month") +
+  facet_wrap(~ stock)  
