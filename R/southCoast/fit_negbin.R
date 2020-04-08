326591f060c8305076d1f068f84d3867c23c706b
CamFreshwater <ccfreshwater@gmail.com>
2020-04-08 10:48:54 -0700

    continue exp w/ neg binomial but reject for insufficiently heavy tailed

--- R/southCoast/fit_negbin.R
+++ R/southCoast/fit_negbin.R
@@ -1,47 +1,61 @@
-## Tweedie model fit
-# March 20, 2020
-# Fit tweedie model to CPUE data from WCVI troll fishery
+## Neg bin model fit
+# April 6, 2020
+# Fit neg binomial model to catch data from WCVI troll fishery
+# Persistently underperforms compared to CPUE model w/ tweedie; don't use for 
+# now; should probably include a zero inflated or heavy-tailed version
 
 library(tidyverse)
 library(TMB)
 
 catch <- readRDS(here::here("data", "gsiCatchData", "commTroll",
                                  "dailyCatch_WCVI.rds")) %>% 
-  filter(!is.na(cpue)) %>% 
+  filter(!is.na(boatDays),
+         !boatDays == "0",
+         !month == "7") %>% 
+  rename(eff = boatDays) %>% 
   mutate(reg = factor(catchReg),
          area = as.factor(area),
          month = as.factor(month),
          month_n = as.numeric(month),
          year = as.factor(year),
-         log_cpue = log(cpue + 0.0001)) %>% 
-  rename(eff = boatDays) %>% 
+         log_cpue = log(cpue + 0.0001),
+         eff_z = as.numeric(scale(eff)),
+         eff_z2 = eff_z^2) %>% 
   arrange(catchReg, month)
 hist(catch$cpue)
-hist(catch$log_cpue)
-
-
-# helper function for inverse logit of p to check par estimates
-inv_logit <- function(x) {
-  1.0 / (1.0 + exp(-x)) + 1.0
-}
-
+hist(catch$catch)
+hist(catch$eff_z2)
+plot(catch ~ eff, data = catch)
 
 # Prep data to pass to model
-y_obs <- catch$cpue
 yr_vec <- as.numeric(as.factor(as.character(catch$year))) - 1
 
 # model matrix for fixed effects
-fix_mm <- model.matrix(~ catchReg + month, catch)
+fix_mm <- model.matrix(~ -1 + (catchReg : month) + eff_z + eff_z2, catch)
 # model matrix for predictions
-mm_pred <- fix_mm %>% 
-  unique() 
-ord_mat <- mm_pred %>% 
-  apply(., 1, sum) %>%
-  sort() %>% 
-  names()
-mm_pred <- mm_pred[ord_mat, ]
+# mm_pred1 <- fix_mm[,-c(1,2)] %>% 
+#   unique() 
+# mm_pred <- cbind(mm_pred1,
+#                  eff_z = rep(0, n = nrow(mm_pred1)),
+#                  eff_z2 = rep(0, n = nrow(mm_pred1)))
 
-data <- list(y1_i = catch$cpue,
+# Factor key of unique combinations to generate predictions
+fac_key <- catch %>%
+  select(catchReg, month) %>%
+  distinct() %>%
+  mutate(facs = paste(catchReg, month, sep = "_"),
+         # facs = fct_relevel(facs, c(as.character(facs))),
+         facs = fct_reorder2(facs, catchReg, 
+                             desc(as.numeric(as.character(month)))),
+         facs_n = as.numeric(as.factor(facs))
+  ) %>%
+  arrange(facs_n)
+mm_pred <- model.matrix(~ facs - 1, fac_key) %>% 
+  cbind(eff_z = rep(0, n = nrow(.)),
+        eff_z2 = rep(0, n = nrow(.)),
+        .)
+
+data <- list(y1_i = catch$catch,
              X1_ij = fix_mm,
              factor1k_i = yr_vec,
              nk1 = length(unique(yr_vec)),
@@ -49,31 +63,26 @@ data <- list(y1_i = catch$cpue,
 )
 
 # Fit simple model to initialize tmb
-m1 <- lm(log_cpue ~ catchReg + month, data = catch)
+# m1 <- lm(log(catch + 0.0001) ~ catchReg + month + eff_z + eff_z2, data = catch)
+m1 <- lm(log(catch + 0.0001) ~ -1 + (catchReg : month) + eff_z + eff_z2, data = catch)
 
 parameters = list(
   b1_j = coef(m1) + rnorm(length(coef(m1)), 0, 0.01),
   log_phi = log(1.5),
-  logit_p = boot::logit(0.8),
   z1_k = rep(0, length(unique(yr_vec))),
   log_sigma_zk1 = log(0.25)
 )
 
-# Factor key of unique combinations to generate predictions
-fac_key <- catch %>% 
-  select(catchReg, month) %>% 
-  distinct() %>% 
-  mutate(facs = paste(catchReg, month, sep = "_"),
-         facs_n = as.numeric(as.factor(facs)))
+
 
 
 # FIT --------------------------------------------------------------------------
 # Compile
-compile(here::here("R", "southCoast", "tmb", "tweedie_cpue_1re.cpp"))
-dyn.load(dynlib(here::here("R", "southCoast", "tmb", "tweedie_cpue_1re")))
+compile(here::here("R", "southCoast", "tmb", "negbin_1re.cpp"))
+dyn.load(dynlib(here::here("R", "southCoast", "tmb", "negbin_1re")))
 
 obj <- MakeADFun(data, parameters, random = c("z1_k"), 
-                 DLL = "tweedie_cpue_1re")
+                 DLL = "negbin_1re")
 
 ## Call function minimizer
 opt <- nlminb(obj$par, obj$fn, obj$gr)
@@ -88,7 +97,6 @@ ssdr
 # PREDICTIONS ------------------------------------------------------------------
 
 ## Plot predictions
-N <- nrow(catch)
 log_pred_fe <- ssdr[rownames(ssdr) %in% "log_prediction", ]
 pred_ci <- data.frame(log_pred_est = log_pred_fe[ , "Estimate"],
                       log_pred_se =  log_pred_fe[ , "Std. Error"]) %>%
@@ -102,11 +110,8 @@ pred_ci <- data.frame(log_pred_est = log_pred_fe[ , "Estimate"],
   left_join(., fac_key, by = "facs_n") 
 
 ggplot() +
-  # geom_point(data = catch %>% filter(!cpue == 0), 
-  #            aes(x = as.factor(month), y = log(cpue)), shape = 21, 
-  #            alpha = 0.2) +
-  geom_boxplot(data = catch %>% filter(!cpue == 0), 
-             aes(x = as.factor(month), y = log(cpue)), alpha = 0.2) +
+  geom_boxplot(data = catch %>% filter(!catch == 0), 
+               aes(x = as.factor(month), y = log(catch)), alpha = 0.2) +
   geom_pointrange(data = pred_ci, aes(x = as.factor(month), y = log_pred_est,
                                       ymin = log_pred_low, 
                                       ymax = log_pred_up)) +
@@ -114,10 +119,16 @@ ggplot() +
   facet_wrap(~catchReg)
 
 ggplot() +
-  geom_boxplot(data = catch, aes(x = as.factor(month), y = cpue), alpha = 0.2) +
-  # geom_point(data = catch, aes(x = as.factor(month), y = cpue), shape = 21, 
-  #            alpha = 0.2) +
+  geom_boxplot(data = catch, aes(x = as.factor(month), y = catch), 
+               alpha = 0.2) +
   geom_pointrange(data = pred_ci, aes(x = as.factor(month), y = pred_est,
                                       ymin = pred_low, ymax = pred_up)) +
   ggsidekick::theme_sleek() +
   facet_wrap(~catchReg)
+
+ggplot(catch) +
+  geom_point(aes(x = eff_z, y = catch), 
+             alpha = 0.2) +
+  stat_function(fun = function(x) exp(4.3 + 1.91*x + (-0.28*(x^2))))
+
+xx <- rnbinom(10000, mu = pred_ci$pred_est, size = exp(0.154))
