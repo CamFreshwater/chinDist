e45da1566300450131570924f5ce4c81e38be157
CamFreshwater <ccfreshwater@gmail.com>
2020-10-26 15:36:00 -0700

    generate combined model predictions using stockseasonr

--- R/southCoast/fit_combined_splines.R
+++ R/southCoast/fit_combined_splines.R
@@ -35,7 +35,7 @@ rec_catch <- readRDS(here::here("data", "gsiCatchData", "rec",
       region == "QCaJS" ~ 8, 
       region %in% c("JdFS", "NSoG", "SSoG")  ~ 9
     ),
-    region = fct_relevel(region, "QCaJS", "NSoG", "SSoG", "JdFS")
+    region = fct_relevel(region, "QCaJS", "JdFS", "NSoG", "SSoG")
   ) %>% 
   # drop areas with fewer than 10 datapoints (month-year-area observation = 1)
   group_by(area_n) %>% 
@@ -142,7 +142,7 @@ clean_comp <- function(grouping_col, raw_data, ...) {
     distinct()
   if (raw_data$gear[1] == "sport") {
     temp %>%
-      mutate(region = fct_relevel(region, "QCaJS", "NSoG", "SSoG", "JdFS"))
+      mutate(region = fct_relevel(region, "QCaJS", "JdFS", "NSoG", "SSoG"))
   } else {
     temp
   }
@@ -166,184 +166,20 @@ full_dat <- tibble(
     catch_data = list(comm_catch, rec_catch, comm_catch, rec_catch)
   )
 
+# saveRDS(full_dat, here::here("data-raw", "combined_model_inputs.RDS"))
+
 
 ## PREP MODEL INPUTS -----------------------------------------------------------
 
-# catch_dat <- full_dat$catch_data[[4]]
-# comp_dat <- full_dat$comp_long[[4]]
-
-# function to generate TMB data inputs from wide dataframes
-gen_tmb <- function(catch_dat, comp_dat) {
-  ## catch data
-  yr_vec_catch <- as.numeric(as.factor(as.character(catch_dat$year))) - 1
-  
-  #generate model matrix based on GAM with spline type a function of months 
-  #retained
-  months1 <- unique(catch_dat$month_n)
-  spline_type <- ifelse(max(months1) == 12, "cc", "tp")
-  n_knots <- ifelse(max(months1) == 12, 4, 3)
-
-  m1 <- gam(catch ~
-              area + s(month_n, bs = spline_type, k = n_knots, by = area) +
-              offset,
-            data = catch_dat,
-            family = nb)
-  fix_mm_catch <- predict(m1, type = "lpmatrix")
-
-  # position of offset vector
-  offset_pos <- grep("^offset$", colnames(fix_mm_catch))
-  
-  # generic data frame that is skeleton for both comp and catch predictions
-  if (comp_dat$gear[1] == "sport") {
-    pred_dat <- group_split(comp_dat, region) %>% 
-      map_dfr(., function (x) {
-        expand.grid(
-          month_n = seq(min(x$month_n), 
-                        max(x$month_n),
-                        by = 0.1),
-          region = unique(x$region)
-        )
-      }) 
-  } else {
-    pred_dat <- expand.grid(
-      month_n = seq(min(comp_dat$month_n), 
-                    max(comp_dat$month_n),
-                    by = 0.1),
-      region = unique(comp_dat$region)
-    )
-  }
-  # list of strata from composition dataset to retain in catch predictive model 
-  # to ensure aggregate abundance can be calculated
-  comp_strata <- unique(paste(pred_dat$month_n, pred_dat$region, sep = "_"))
-  
-  # make predictive model matrix including null values for effort
-  pred_dat_catch <- expand.grid(
-    month_n = seq(min(catch_dat$month_n),
-                  max(catch_dat$month_n),
-                  by = 0.1),
-    area = unique(catch_dat$area),
-    offset = mean(catch_dat$offset)
-  ) %>%
-    left_join(., 
-              catch_dat %>% select(region, area) %>% distinct(), 
-              by = "area") %>% 
-    mutate(strata = paste(month_n, region, sep = "_")) %>% 
-    filter(strata %in% comp_strata) %>% 
-    arrange(region) %>%
-    #convoluted fuckery to ensure ordering is correct for key
-    mutate(month = as.factor(month_n),
-           order = row_number(),
-           reg_month = paste(region, month, sep = "_"),
-           reg_month_f = fct_reorder(factor(reg_month), order)) %>% 
-    select(-order, -reg_month, -strata) %>%
-    distinct()
-  pred_mm_catch <- predict(m1, pred_dat_catch, type = "lpmatrix")
-  
-  # construct factor key for regional aggregates associated with areas
-  grouping_vec <- as.numeric(pred_dat_catch$reg_month_f) - 1
-  grouping_key <- unique(grouping_vec)
-
-  ## composition data
-  gsi_wide <- comp_dat %>% 
-    pivot_wider(., names_from = agg, values_from = agg_prob) %>%
-    mutate_if(is.numeric, ~replace_na(., 0.00001))
-  
-  obs_comp <- gsi_wide %>% 
-    select(-c(sample_id:nn)) %>% 
-    as.matrix()  
-  
-  yr_vec_comp <- as.numeric(gsi_wide$year) - 1
-  
-  months2 <- unique(gsi_wide$month_n)
-  spline_type <- ifelse(max(months2) == 12, "cc", "tp")
-  n_knots <- ifelse(max(months2) == 12, 4, 3)
-  # response variable doesn't matter, since not fit
-  m2 <- gam(rep(0, length.out = nrow(gsi_wide)) ~ 
-              region + s(month_n, bs = spline_type, k = n_knots, by = region), 
-            data = gsi_wide)
-  fix_mm_comp <- predict(m2, type = "lpmatrix")
-  
-  pred_mm_comp <- predict(m2, pred_dat, type = "lpmatrix")
-
-  ## region-stock combinations with 0 observations to map
-  comp_map <- comp_dat %>%
-    group_by(region, agg) %>%
-    complete(., region, nesting(agg)) %>% 
-    summarize(total_obs = sum(agg_prob), .groups = "drop") %>% 
-    filter(is.na(total_obs)) %>% 
-    select(agg, region)
-  
-  #input data
-  data = list(
-    #abundance input data
-    y1_i = catch_dat$catch,
-    X1_ij = fix_mm_catch,
-    factor1k_i = yr_vec_catch,
-    nk1 = length(unique(yr_vec_catch)),
-    X1_pred_ij = pred_mm_catch,
-    pred_factor2k_h = grouping_vec,
-    pred_factor2k_levels = grouping_key,
-    #composition input data
-    y2_ig = obs_comp,
-    X2_ij = fix_mm_comp,
-    factor2k_i = yr_vec_comp,
-    nk2 = length(unique(yr_vec_comp)),
-    X2_pred_ij = pred_mm_comp
-  )
-  
-  #input parameter initial values
-  pars = list(
-    #abundance parameters
-    b1_j = coef(m1) + rnorm(length(coef(m1)), 0, 0.01),
-    log_phi = log(1.5),
-    z1_k = rep(0, length(unique(yr_vec_catch))),
-    log_sigma_zk1 = log(0.25),
-    #composition parameters
-    b2_jg = matrix(runif(ncol(fix_mm_comp) * ncol(obs_comp), 0.1, 0.9), 
-                   nrow = ncol(fix_mm_comp), 
-                   ncol = ncol(obs_comp)),
-    z2_k = rep(0, times = length(unique(yr_vec_comp))),
-    log_sigma_zk2 = log(0.5)
-  )
-  
-  # mapped values (not estimated)
-  # offset for effort
-  pars$b1_j[offset_pos] <- 1
-  b1_j_map <- seq_along(pars$b1_j)
-  b1_j_map[offset_pos] <- NA
-  tmb_map <- list(b1_j = as.factor(b1_j_map))
-  # offset for composition parameterss 
-  if (!is.na(comp_map$agg[1])) {
-    temp_betas <- pars$b2_jg
-    for (i in 1:nrow(comp_map)) {
-      offset_stock_pos <- grep(paste(comp_map$agg[1], collapse="|"), 
-                               colnames(obs_comp))
-      offset_region_pos <- grep(paste(comp_map$region[1], collapse="|"), 
-                                colnames(fix_mm_comp))
-      for (j in seq_len(ncol(fix_mm_comp))) {
-        for (k in seq_len(ncol(obs_comp))) {
-          if (j %in% offset_region_pos & k %in% offset_stock_pos) {
-            pars$b2_jg[j, k] <- 0.00001
-            temp_betas[j, k] <- NA
-          }
-        }
-      }
-    }
-    comp_map_pos <- which(is.na(as.vector(temp_betas)))
-    b2_jg_map <- seq_along(pars$b2_jg)
-    b2_jg_map[comp_map_pos] <- NA
-    tmb_map <- c(tmb_map, list(b2_jg = as.factor(b2_jg_map)))
-  }
-
-  list("data" = data, "pars" = pars, "comp_wide" = gsi_wide,
-       "pred_dat_catch" = pred_dat_catch, "pred_dat_comp" = pred_dat,
-       "tmb_map" = tmb_map)
-}
-
-
-#add tmb data assuming average effort predictions (for month specific look at
-# non-spline code)
-tmb_list <- map2(full_dat$catch_data, full_dat$comp_long, .f = gen_tmb)
+# add tmb data assuming average effort predictions
+# note that these outputs are generated automatically in fit_stockseason
+# but aren't retained and are necessary for the current suite of plotting 
+# functions
+tmb_list <- purrr::pmap(list(catch_dat = full_dat$catch_data, 
+                             comp_dat = full_dat$comp_long), 
+                        .f = stockseasonr::gen_tmb,
+                        random_walk = TRUE,
+                        model_type = "integrated")
 
 # join all data into a tibble then generate model inputs
 dat <- full_dat %>% 
@@ -356,35 +192,17 @@ dat <- full_dat %>%
     pred_dat_comp = purrr::map(tmb_list, ~ .$pred_dat_comp)
   ) 
 
-
 ## FIT MODELS ------------------------------------------------------------------
-
-compile(here::here("src", "nb_dirichlet_1re.cpp"))
-dyn.load(dynlib(here::here("src", "nb_dirichlet_1re")))
-
-fit_mod <- function(tmb_data, tmb_pars, tmb_map, nlminb_loops = 2) {
-  obj <- MakeADFun(tmb_data, tmb_pars, map = tmb_map,
-                   #dat$tmb_data[[4]], dat$tmb_pars[[4]], map = dat$tmb_map[[4]],
-                   random = c("z1_k", "z2_k"), 
-                   DLL = "nb_dirichlet_1re")
-  
-  opt <- nlminb(obj$par, obj$fn, obj$gr)
-  if (nlminb_loops > 1) {
-    for (i in seq(2, nlminb_loops, length = max(0, nlminb_loops - 1))) {
-      opt <- nlminb(start = opt$par, objective = obj$fn, gradient = obj$gr)
-    }
-  }
-  sdreport(obj)
-  # sdr <- sdreport(obj)
-  # ssdr <- summary(sdr)
-}
-
-# join all data into a tibble then generate model inputs
 dat2 <- dat %>%
-  mutate(sdr = purrr::pmap(list(tmb_data, tmb_pars, tmb_map), 
-                           .f = fit_mod, nlminb_loops = 2),
-         ssdr = purrr::map(sdr, summary)
-         )
+  mutate(sdr = purrr::pmap(list(catch_dat = full_dat$catch_data, 
+                                comp_dat = full_dat$comp_long), 
+                           .f = stockseasonr::fit_stockseason, 
+                           random_walk = TRUE,
+                           model_type = "integrated",
+                           nlminb_loops = 2, 
+                           silent = FALSE))
+dat2$ssdr <- purrr::map(dat2$sdr, summary)
+
 
 saveRDS(dat2 %>% select(-sdr),
         here::here("generated_data", "model_fits", "combined_model_dir.RDS"))
@@ -404,12 +222,17 @@ pred_dat <- dat2 %>%
     #predictions assuming mean effort
     abund_pred_ci = suppressWarnings(pmap(list(comp_long, pred_dat_comp, ssdr), 
                                           .f = gen_abund_pred)),
+    cpue_pred_ci = pmap(list(pred_dat_catch, comp_long, ssdr), 
+                        gen_abund_pred_area),
     comp_pred_ci = suppressWarnings(pmap(list(comp_long, pred_dat_comp, ssdr), 
                         .f = gen_comp_pred)),
     raw_prop = purrr::map2(comp_long, comp_wide, make_raw_prop_dat),
-    raw_abund = pmap(list(catch_data, raw_prop), .f = make_raw_abund_dat)
+    raw_abund_area = pmap(list(catch_data, raw_prop), .f = make_raw_abund_dat,
+                          spatial_scale = "area"),
+    raw_abund_reg = pmap(list(catch_data, raw_prop), .f = make_raw_abund_dat,
+                         spatial_scale = "region")
     ) %>% 
-  select(dataset:catch_data, abund_pred_ci:raw_abund) %>% 
+  select(dataset:catch_data, abund_pred_ci:raw_abund_reg) %>% 
   mutate(
     comp_pred_ci = map2(grouping_col, comp_pred_ci, .f = stock_reorder),
     raw_prop = map2(grouping_col, raw_prop, .f = stock_reorder)
@@ -418,7 +241,6 @@ saveRDS(pred_dat, here::here("generated_data",
                              "combined_model_predictions.RDS"))
 pred_dat <- readRDS(here::here("generated_data",
                                "combined_model_predictions.RDS"))
-
 
 ## PLOT PREDICTIONS ------------------------------------------------------------
 
@@ -430,6 +252,7 @@ pfma_map <- readRDS(here::here("generated_data", "pfma_map.rds"))
 
 # generic settings
 file_path <- here::here("figs", "model_pred", "combined")
+
 
 ## Plot abundance
 comm1 <- pred_dat %>% 
@@ -459,20 +282,45 @@ pdf(paste(file_path, "pred_abundance_splines.pdf", sep = "/"))
 combo_abund2
 dev.off()
 
-## Composition prediction
+
+## Plot area abundance with observations
+# combine similar groupings to plot together
+comm_area_cpue <- plot_cpue_area(pred_dat$cpue_pred_ci[[1]], 
+                        pred_dat$raw_abund_area[[1]])
+rec_area_cpue <- plot_cpue_area(pred_dat$cpue_pred_ci[[2]], 
+                       pred_dat$raw_abund_area[[2]])
+
+
+## Composition spline prediction
 comp_plots <- map2(pred_dat$comp_pred_ci, pred_dat$raw_prop, plot_comp, 
                    raw = TRUE)
 comp_plots_fix <- map2(pred_dat$comp_pred_ci, pred_dat$raw_prop, plot_comp, 
                        raw = TRUE, facet_scales = "fixed")
-comp_plots_fix2 <- map2(pred_dat$comp_pred_ci, pred_dat$raw_prop, plot_comp, 
-                        raw = FALSE, facet_scales = "fixed")
 
 pdf(paste(file_path, "composition_splines.pdf", sep = "/"))
 comp_plots
 comp_plots_fix
 dev.off()
 
-## Stock-specific CPUE predictions
+
+## Composition area prediction
+combine_data <- function(in_col = c("pst_agg", "reg1")) {
+  pred_dat %>% 
+    filter(grouping_col == in_col) %>% 
+    select(dataset, grouping_col, comp_pred_ci) %>% 
+    unnest(., cols = c(comp_pred_ci))
+}  
+pst_comp_pred <- combine_data(in_col = "pst_agg")
+plot_comp_stacked(pst_comp_pred, grouping_col = "pst_agg")
+
+comp_area_plots <- map2(pred_dat$comp_pred_ci, pred_dat$grouping_col, 
+                        plot_comp_stacked)
+pdf(paste(file_path, "composition_stacked.pdf", sep = "/"))
+comp_area_plots
+dev.off()
+
+
+## Stock-specific catch predictions
 ss_abund_plots_free <- map2(pred_dat$comp_pred_ci, pred_dat$raw_abund, 
                             plot_ss_abund, raw = FALSE)
 ss_abund_plots_fix <- map2(pred_dat$comp_pred_ci, pred_dat$raw_abund, 
@@ -534,6 +382,16 @@ ss_abund_plots_free[[4]] +
   labs(y = "Predicted Recreational Catch Index")
 dev.off()
 
+# supplemental figures
+png(here::here("figs", "ms_figs", "comm_cpue_fits.png"), res = 400, units = "in",
+    height = 5.5, width = 7)
+comm_area_cpue
+dev.off()
+
+png(here::here("figs", "ms_figs", "rec_cpue_fits.png"), res = 400, units = "in",
+    height = 5.5, width = 7)
+rec_area_cpue
+dev.off()
 
 ## Random summary data ---------------------------------------------------------
 
@@ -617,8 +475,10 @@ write.csv(comp_n, here::here("figs", "ms_figs", "tableS2_comp_samples.csv"),
 # CPUE figs
 plot_cpue <- function(dat) {
   dat %>% 
-    mutate(cpue = catch / eff) %>% 
-    ggplot(., aes(x = month, y = cpue, 
+    mutate(cpue = catch / eff,
+           log_cpue = log(catch) / log(eff),
+           log_cpue2 = log(cpue)) %>% 
+    ggplot(., aes(x = month, y = log_cpue2, 
                   fill = fct_reorder(region_c, as.numeric(region)))) +
     geom_point(shape = 21) +
     scale_fill_manual(name = "Region", values = pal) +
