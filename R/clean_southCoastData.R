6af9d0f286ca8bb30105c0c9c7e050b41d6cd021
CamFreshwater <ccfreshwater@gmail.com>
2020-11-16 16:39:46 -0800

    update cleaning scripts

--- R/clean_southCoastData.R
+++ R/clean_southCoastData.R
@@ -1,120 +1,117 @@
 ## Clean catch/effort data from southcoast 
-# Commercial data provided by Bryan Rusch Aug 19, 2019 as Access database
-# Note: the tables within the access database are not joined, limiting the
-# utility of multitable queries; to ensure repeatability the majority of data
-# cleaning occurs after tables have been imported to R.
-# Recreational data provided by Wilf Luedke May 27, 2020 as Excel file
-# Note: information on data selection for rec is in .txt file in directory
+# Commercial data provided by BR Aug 19, 2019 as Access database which
+# cannot be hosted publicly; imported csv attached here
+# Recreational data provided by WL May 27, 2020
 
 library(RODBC); library(tidyverse); library(ggplot2)
 
-
-## CLEAN COMMERCIAL ------------------------------------------------------------
-
-# Access database saved locally to work computer
-file_path <- "C:/Users/FRESHWATERC/Documents/chinook/southCoastDatabase/southCoastGSICatch.accdb"
-con <- odbcConnectAccess2007(file_path)
-
-# Check out table names
-sqlTables(con, tableType = "TABLE")$TABLE_NAME
-
-## Import various datatables
-# all gsi samples
-sampInvQry <- "SELECT *
-               FROM [Sample Inventory]"
-sampInv <- sqlQuery(con, sampInvQry) %>% 
-  select(-F14, -F15, -F16) %>% 
-  rename_all(list(~make.names(.))) #get rid of spaces in col names
-trimSampInv <- sampInv %>% 
-  select(Sample.ID, Vial.ID, Year, Fishery, Catch.Region, Catch.Area, 
-         Month.Code, Month.Name, Lab.Reported.N)
-
-# stock composition data
-stockCompQry <- "SELECT *
-                 FROM [Stock Level Results] 
-                 INNER JOIN [Stocks with Region Codes]
-                 ON [Stock Level Results].[Stock Code] = 
-                  [Stocks with Region Codes].[Stock Code];"
-stockComp <- sqlQuery(con, stockCompQry) %>% 
-  rename_all(list(~make.names(.))) %>% 
-  left_join(., trimSampInv, by = "Sample.ID") %>% 
-  select(Sample.ID:SD, Region1Code:Lab.Reported.N) %>% 
-  rename(catchReg = Catch.Region, month = Month.Code, year = Year) %>% 
-  mutate(catchReg = as.character(catchReg)) %>% 
-  filter(Fishery == "Area G Com") 
-yrs <- unique(stockComp$year)
-
-# confirm one sample collected per catch region, year, and month
-# dum <- stockComp %>%
-#   select(-c(Stock.Code:Vial.ID)) %>%
-#   distinct() %>%
-#   group_by(Catch.Region, Year, Month.Name) %>%
-#   tally()
-
-# region key to reference stocks at different roll ups
-reg1Qry <- "SELECT *
-            FROM [Region 1 Stock Names];"
-reg1 <- sqlQuery(con, reg1Qry) %>% 
-  select(-ID)
-reg3Qry <- "SELECT *
-            FROM [Region 3 Stock Names];"
-reg3 <- sqlQuery(con, reg3Qry) %>% 
-  select(-ID)
-# frNameQry <- "SELECT *
-#               FROM [Fraser Stock Grouping Names]"
-reg2Qry <- "SELECT *
-           FROM [Stocks with Region Codes]
-           LEFT JOIN [Region 2 Stock Names]
-           ON [Stocks with Region Codes].Region2Code = 
-              [Region 2 Stock Names].Region2Code;"
-regKey <- sqlQuery(con, reg2Qry) %>%
-  rename_all(list(~make.names(.))) %>%
-  left_join(., reg1, by = "Region1Code") %>% 
-  left_join(., reg3, by = "Region3Code") %>% 
-  mutate(Stock = Stock.Name) %>% 
-  select(Stock.Code, Stock, Region2Name, Region1Name, Region3Name, 
-         Region1Code:FraserGroupCode)
-head(regKey)
-
-trimRegKey <- regKey %>% 
-  mutate(Stock = as.character(Stock)) %>% 
-  select(Stock, Region1Name, Region2Name, Region3Name)
-
-# write.csv(regKey, here::here("data", "southcoastStockKey.csv"), row.names = F)
-
-# all catch/effort data
-catchQry <- "SELECT [Area G FOS Catch Estimates].ESTIMATE_TYPE, 
-                    [Area G FOS Catch Estimates].LICENCE_AREA, 
-                    [Area G FOS Catch Estimates].OPNG_CAT, 
-                    [Area G FOS Catch Estimates].OPNG_DESC, 
-                    [Area G FOS Catch Estimates].TARGETS_CHINOOK, 
-                    [Area G FOS Catch Estimates].STAT_WEEK, 
-                    [Area G FOS Catch Estimates].FISHING_DATE, 
-                    Month([FISHING_DATE]) AS [FISHING MONTH], 
-                    Year([FISHING_DATE]) AS [FISHING YEAR], 
-                    [Area G FOS Catch Estimates].MGMT_AREA, 
-                    [Area G FOS Catch Estimates].AREA_NAME, 
-                    [Management Regions].CATCH_REGION, 
-                    [Area G FOS Catch Estimates].HRS_OPEN, 
-                    [Area G FOS Catch Estimates].VESSELS_OP, 
-                    [Area G FOS Catch Estimates].CHINOOK_KEPT, 
-                    [Area G FOS Catch Estimates].CHINOOK_RELD, 
-                    [Area G FOS Catch Estimates].COMMENTS
-               FROM [Area G FOS Catch Estimates] 
-               INNER JOIN [Management Regions] 
-               ON [Area G FOS Catch Estimates].MGMT_AREA = 
-                  [Management Regions].[Managment Area]
-               WHERE ((([Area G FOS Catch Estimates].TARGETS_CHINOOK)='Yes'));
-"
-areaCatch <- sqlQuery(con, catchQry) %>% 
-  rename_all(list(~make.names(.))) %>%
-  filter(FISHING.YEAR %in% yrs) %>% 
-  # correct anomalously low vessel operating after looking at FOS
-  mutate(VESSELS_OP = 
-           case_when(
-             FISHING_DATE == "2013-05-15" & MGMT_AREA == "123" ~ 39,
-             TRUE ~ VESSELS_OP)
-         )
+ 
+# ## CLEAN COMMERCIAL ------------------------------------------------------------
+#
+# # Access database saved locally to work computer
+# file_path <- "C:/Users/FRESHWATERC/Documents/chinook/southCoastDatabase/southCoastGSICatch.accdb"
+# con <- odbcConnectAccess2007(file_path)
+# 
+# # Check out table names
+# sqlTables(con, tableType = "TABLE")$TABLE_NAME
+# 
+# ## Import various datatables
+# # all gsi samples
+# sampInvQry <- "SELECT *
+#                FROM [Sample Inventory]"
+# sampInv <- sqlQuery(con, sampInvQry) %>% 
+#   select(-F14, -F15, -F16) %>% 
+#   rename_all(list(~make.names(.))) #get rid of spaces in col names
+# trimSampInv <- sampInv %>% 
+#   select(Sample.ID, Vial.ID, Year, Fishery, Catch.Region, Catch.Area, 
+#          Month.Code, Month.Name, Lab.Reported.N)
+# 
+# # stock composition data
+# stockCompQry <- "SELECT *
+#                  FROM [Stock Level Results] 
+#                  INNER JOIN [Stocks with Region Codes]
+#                  ON [Stock Level Results].[Stock Code] = 
+#                   [Stocks with Region Codes].[Stock Code];"
+# stockComp <- sqlQuery(con, stockCompQry) %>% 
+#   rename_all(list(~make.names(.))) %>% 
+#   left_join(., trimSampInv, by = "Sample.ID") %>% 
+#   select(Sample.ID:SD, Region1Code:Lab.Reported.N) %>% 
+#   rename(catchReg = Catch.Region, month = Month.Code, year = Year) %>% 
+#   mutate(catchReg = as.character(catchReg)) %>% 
+#   filter(Fishery == "Area G Com") 
+# yrs <- unique(stockComp$year)
+# 
+# # confirm one sample collected per catch region, year, and month
+# # dum <- stockComp %>%
+# #   select(-c(Stock.Code:Vial.ID)) %>%
+# #   distinct() %>%
+# #   group_by(Catch.Region, Year, Month.Name) %>%
+# #   tally()
+# 
+# # region key to reference stocks at different roll ups
+# reg1Qry <- "SELECT *
+#             FROM [Region 1 Stock Names];"
+# reg1 <- sqlQuery(con, reg1Qry) %>% 
+#   select(-ID)
+# reg3Qry <- "SELECT *
+#             FROM [Region 3 Stock Names];"
+# reg3 <- sqlQuery(con, reg3Qry) %>% 
+#   select(-ID)
+# # frNameQry <- "SELECT *
+# #               FROM [Fraser Stock Grouping Names]"
+# reg2Qry <- "SELECT *
+#            FROM [Stocks with Region Codes]
+#            LEFT JOIN [Region 2 Stock Names]
+#            ON [Stocks with Region Codes].Region2Code = 
+#               [Region 2 Stock Names].Region2Code;"
+# regKey <- sqlQuery(con, reg2Qry) %>%
+#   rename_all(list(~make.names(.))) %>%
+#   left_join(., reg1, by = "Region1Code") %>% 
+#   left_join(., reg3, by = "Region3Code") %>% 
+#   mutate(Stock = Stock.Name) %>% 
+#   select(Stock.Code, Stock, Region2Name, Region1Name, Region3Name, 
+#          Region1Code:FraserGroupCode)
+# head(regKey)
+# 
+# trimRegKey <- regKey %>% 
+#   mutate(Stock = as.character(Stock)) %>% 
+#   select(Stock, Region1Name, Region2Name, Region3Name)
+# 
+# # write.csv(regKey, here::here("data", "southcoastStockKey.csv"), row.names = F)
+# 
+# # all catch/effort data
+# catchQry <- "SELECT [Area G FOS Catch Estimates].ESTIMATE_TYPE, 
+#                     [Area G FOS Catch Estimates].LICENCE_AREA, 
+#                     [Area G FOS Catch Estimates].OPNG_CAT, 
+#                     [Area G FOS Catch Estimates].OPNG_DESC, 
+#                     [Area G FOS Catch Estimates].TARGETS_CHINOOK, 
+#                     [Area G FOS Catch Estimates].STAT_WEEK, 
+#                     [Area G FOS Catch Estimates].FISHING_DATE, 
+#                     Month([FISHING_DATE]) AS [FISHING MONTH], 
+#                     Year([FISHING_DATE]) AS [FISHING YEAR], 
+#                     [Area G FOS Catch Estimates].MGMT_AREA, 
+#                     [Area G FOS Catch Estimates].AREA_NAME, 
+#                     [Management Regions].CATCH_REGION, 
+#                     [Area G FOS Catch Estimates].HRS_OPEN, 
+#                     [Area G FOS Catch Estimates].VESSELS_OP, 
+#                     [Area G FOS Catch Estimates].CHINOOK_KEPT, 
+#                     [Area G FOS Catch Estimates].CHINOOK_RELD, 
+#                     [Area G FOS Catch Estimates].COMMENTS
+#                FROM [Area G FOS Catch Estimates] 
+#                INNER JOIN [Management Regions] 
+#                ON [Area G FOS Catch Estimates].MGMT_AREA = 
+#                   [Management Regions].[Managment Area]
+#                WHERE ((([Area G FOS Catch Estimates].TARGETS_CHINOOK)='Yes'));
+# "
+# areaCatch <- sqlQuery(con, catchQry) %>% 
+#   rename_all(list(~make.names(.))) %>%
+#   filter(FISHING.YEAR %in% yrs) %>% 
+#   # correct anomalously low vessel operating after looking at FOS
+#   mutate(VESSELS_OP = 
+#            case_when(
+#              FISHING_DATE == "2013-05-15" & MGMT_AREA == "123" ~ 39,
+#              TRUE ~ VESSELS_OP)
+#          )
 # write.csv(areaCatch, here::here("data", "gsiCatchData", "commTroll",
 #                                 "fosCatch.csv"), 
 #           row.names = FALSE)
@@ -179,10 +176,10 @@ unpub_strata <- rec_catch %>%
   filter(STATUS != "Published Estimate - Full Month") %>%
   pull(strata)
 
-rec_catch %>%
-  filter(STATUS == "Published Estimate - Full Month",
-         strata %in% unpub_strata) %>% 
-  pull(strata)
+# rec_catch %>%
+#   filter(STATUS == "Published Estimate - Full Month",
+#          strata %in% unpub_strata) %>% 
+#   pull(strata)
 #none seem to be cross referenced so retain both
 
 # clean
